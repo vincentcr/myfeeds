80e2754a2366e4d4f2ca2775a930231a39c5a60b
Vincent Côté-Roy <vincentcr@gmail.com>
2015-12-06 07:45:24 -0500

    remove test echo

--- api/db/001-force-auth.sh
+++ api/db/001-force-auth.sh
@@ -3,5 +3,3 @@
 FORCE_AUTH="host all  all 0.0.0.0/0 md5"
 sed -i -e "s|host *all *all *0\.0\.0\.0/0 *trust|${FORCE_AUTH}|g" /var/lib/postgresql/data/pg_hba.conf
 echo "${FORCE_AUTH}" >> /var/lib/postgresql/data/pg_hba.conf
-
-echo "HELLO WORLD!!!!"
